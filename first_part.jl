7d727ac76ca2b1dba39c0ca5089a1ad5e41c8bae
mrizaarseven98 <95386756+mrizaarseven98@users.noreply.github.com>
2021-12-18 12:52:21 +0100

    XGB Model added

--- first_part.jl
+++ first_part.jl
@@ -2,11 +2,10 @@
  begin
     #Imports required packages
     using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-    using CSV, DataFrames, MLJ, NearestNeighborModels, MLCourse, Random, Plots,MLJLinearModels
+    using CSV, DataFrames, MLJ, NearestNeighborModels, MLCourse, Random, Plots,MLJLinearModels, OpenML, Flux
 
     #Imports training, test and a sample submission data
     training = CSV.read(joinpath(@__DIR__, "Data", "trainingdata.csv"),DataFrame)
-    submission = CSV.read(joinpath(@__DIR__, "Data", "sample_submission.csv"),DataFrame)
     testing=CSV.read(joinpath(@__DIR__, "Data", "testdata.csv"),DataFrame)
 end
 
@@ -17,28 +16,30 @@ begin
 end
 
 
-#KNNRegressor
+#KNNRegressor: ATTENTION: MAY NOT BE THE SAME PARAMETERS FROM SUBMISSION 1
 begin
     model1 = KNNRegressor()
     self_tuning_kNN = TunedModel(model = model1,
-                                   resampling = CV(nfolds =20),
+                                   resampling = CV(nfolds =5),
                                    tuning = Grid(),
-                                   range = range(model1, :K, values = 1:50),
+                                   range = range(model1, :K, values = 1:20),
                                    measure = rmse)
     self_tuning_kNN_mach = machine(self_tuning_kNN,
                                select(training1, Not(:precipitation_nextday)),
                                training1.precipitation_nextday) |> fit!
 end
 
-#RidgeRegressor
+
+
+#RidgeRegressor: ATTENTION: MAY NOT BE THE SAME PARAMETERS FROM SUBMISSION 2
 begin
     model2 = RidgeRegressor()
     self_tuning_ridge = TunedModel(model = model2,
-	                         resampling = CV(nfolds = 20),
+	                         resampling = CV(nfolds = 10),
 	                         tuning = Grid(goal = 200),
 	                         range = range(model2, :lambda,
 									       scale = :log,
-									       lower = 1e-3, upper = 1e4),
+									       lower = 1e-8, upper = 1e-6),
 	                         measure = rmse)
     
     self_tuning_ridge_mach = machine(self_tuning_ridge,
@@ -46,21 +47,9 @@ begin
                                training1.precipitation_nextday) |> fit!
 end
 
-#PolynomialRegressor (Doesn't work for some reason)
-begin
-    model3 = MLCourse.PolynomialRegressor()
-    self_tuning_poly = TunedModel(model = model3,
-                                   resampling = CV(nfolds =10),
-                                   tuning = Grid(),
-                                   range = range(model3, :degree, values = 1:20),
-                                   measure = rmse)
-    
-    self_tuning_poly_mach = machine(self_tuning_poly,
-                               select(training1, Not(:precipitation_nextday)),
-                               training1.precipitation_nextday) |> fit!
-end
 
-#LinearRegressor
+
+#LinearRegressor:
 begin
     model4 = LinearRegressor()
     
@@ -69,6 +58,29 @@ begin
                                select(training1, Not(:precipitation_nextday)),
                                training1.precipitation_nextday) |> fit!
 end
+
+
+
+
+#Tree Based Regressor: Submission 4
+begin
+    using MLJXGBoostInterface,MLJDecisionTreeInterface
+    weather_input = MLJ.transform(machine(OneHotEncoder(drop_last = true),
+	                               select(training1, Not(:precipitation_nextday))) |> fit!,
+                                   select(training1, Not(:precipitation_nextday)))
+
+	xgb = XGBoostRegressor()
+    self_tuning_xgb_mach = machine(TunedModel(model = xgb,
+                            resampling = CV(nfolds = 10),
+                            tuning = Grid(goal=25),
+                            range = [range(xgb, :eta,
+                                           lower = 1e-2, upper = .1, scale = :log),
+                                     range(xgb, :num_round, lower = 50, upper = 500),
+                                     range(xgb, :max_depth, lower = 2, upper = 6)]),
+                  weather_input, training1.precipitation_nextday) |> fit!
+                
+end
+
 
 
 #Function that makes sure the value is a probability (between 0 and 1)
@@ -97,18 +109,19 @@ begin
     pred_ridge= enemy_of_out_of_bounds(predict(self_tuning_ridge_mach, select(training1, Not(:precipitation_nextday))))
     pred_poly= enemy_of_out_of_bounds(predict(self_tuning_poly_mach, select(training1, Not(:precipitation_nextday))))
     pred_linear= enemy_of_out_of_bounds(predict(linear_mach, select(training1, Not(:precipitation_nextday))))
+    pred_xgb= enemy_of_out_of_bounds(predict(self_tuning_xgb_mach, select(training1, Not(:precipitation_nextday))))
 
     #Calculates the RMSEs
     rmse_kNN=rmse(predkNN, training1.precipitation_nextday)
     rmse_ridge=rmse(pred_ridge, training1.precipitation_nextday)
     rmse_poly=rmse(pred_poly, training1.precipitation_nextday)
     rmse_linear=rmse(pred_linear, training1.precipitation_nextday)
+    rmse_pred_xgb=rmse(pred_xgb, training1.precipitation_nextday)
 
     #Creates DataFrame
-    training_errors=DataFrame(Models= ["kNN Regression","Ridge Regression","PolynomialRegression", "LinearRegression"],
-                                                                         Training_RMSE=[rmse_kNN,rmse_ridge,rmse_poly,rmse_linear])
+    training_errors=DataFrame(Models= ["kNN Regression","Ridge Regression","PolynomialRegression", "LinearRegression", "XGB"],
+                                                                         Training_RMSE=[rmse_kNN,rmse_ridge,rmse_poly,rmse_linear, rmse_pred_xgb])
 end
-
 
 
 #Reports of the Various Models
@@ -135,6 +148,10 @@ begin
     #Predictions based on test data for Linear Regressor
     prediction3=enemy_of_out_of_bounds(predict(linear_mach, testing))
     submission3_linear=DataFrame(id=[i[1] for i in enumerate(prediction3)],precipitation_nextday=prediction3)
+
+    #Predictions based on test data for XGB    
+    prediction4=enemy_of_out_of_bounds(predict(self_tuning_xgb_mach, testing))
+    submission4_xgb=DataFrame(id=[i[1] for i in enumerate(prediction4)],precipitation_nextday=prediction4)
 end
 
 
@@ -143,6 +160,7 @@ begin
     CSV.write(joinpath(@__DIR__, "Submission_Data", "submission1_kNN.csv"), submission1_kNN)
     CSV.write(joinpath(@__DIR__, "Submission_Data", "submission2_ridge.csv"), submission2_ridge)
     CSV.write(joinpath(@__DIR__, "Submission_Data", "submission3_linear.csv"), submission3_linear)
+    CSV.write(joinpath(@__DIR__, "Submission_Data", "submission4_xgb.csv"), submission4_xgb)
 end
 
 
