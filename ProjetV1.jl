1d5199ecc6e2d28367c296b01767ec7f50668c48
AlexMlld <alex.maillard74@gmail.com>
2021-12-22 21:17:47 +0100

    Random Forest + test NNR

--- ProjetV1.jl
+++ ProjetV1.jl
@@ -4,13 +4,8 @@ begin
     using MLCourse, Plots, MLJ, DataFrames, Random, CSV, Flux, Distributions,
           StatsPlots, MLJFlux, OpenML
     using Flux: onehot, onehotbatch, logitcrossentropy, reset!, throttle
-    using XGBoost
-    using ScikitLearn
-    using ScikitLearn.GridSearch: GridSearchCV
     Core.eval(Main, :(using MLJ))
-    using ScikitLearn.CrossValidation: cross_val_score
     using OpenML, MLJ, MLJXGBoostInterface, DataFrames, MLJLinearModels, MLJDecisionTreeInterface
-    
 end
 
 begin
@@ -23,8 +18,8 @@ begin
 	                       select(weather_INPUT, Not(:precipitation_nextday)))
 end
 
-
-##Test fonctionnement ScikitLearn et GridSearchCV
+#=
+Test fonctionnement ScikitLearn et GridSearchCV
 begin
     nfold = 5
     num_round = 2
@@ -44,7 +39,7 @@ xgb_params = [learning_rate=[0.1,0.01,0.5],
              colsample_bytree=[0.4,0.7,1]]
 xgb_cv_model = GridSearchCV(xgb,xgb_params,cv =10,n_jobs = -1,verbose =2).fit(select(weather_INPUT, Not(:precipitation_nextday)),weather_INPUT.precipitation_nextday)
 
-##
+=#
 
 begin
 	
@@ -60,6 +55,11 @@ begin
 end
 
 begin
+    pred_xgb= enemy_of_out_of_bounds(predict(m2, select(weather_INPUT, Not([:precipitation_nextday]))))
+    rmse_xgb=rmse(pred_xgb, weather_INPUT.precipitation_nextday)
+end
+
+begin
     function enemy_of_out_of_bounds(prediction)
         for k in enumerate(prediction)
             if k[2]>=1
@@ -71,11 +71,7 @@ begin
         prediction
     end
 end
-pred_xgb= enemy_of_out_of_bounds(predict(m2, select(weather_INPUT, Not([:precipitation_nextday]))))
-begin
-    pred_xgb= enemy_of_out_of_bounds(predict(m2, select(weather_INPUT, Not([:precipitation_nextday]))))
-    rmse_xgb=rmse(pred_xgb, weather_INPUT.precipitation_nextday)
-end
+
 
 
 begin
@@ -141,3 +137,96 @@ xgb_tuned = XGBRegressor(colsample_bytree = 0.4,learning_rate = 0.1,max_depth = 
 =#
 #GridSearchCV
 #https://xgboost.readthedocs.io/en/latest/parameter.html
+
+begin
+    weatherX= MLJ.transform(machine(OneHotEncoder(drop_last = true),
+                            select(weather_INPUT, Not([:precipitation_nextday]))) |> fit!,
+                            select(weather_INPUT, Not(:precipitation_nextday)))
+    weatherX_mach = fit!(machine(Standardizer(), weatherX));
+    data_weatherX = MLJ.transform(weatherX_mach, weatherX)
+    weatherY= weather_INPUT.precipitation_nextday
+    weatherY_mach = fit!(machine(Standardizer(), weatherY));
+    data_weatherY = MLJ.transform(weatherY_mach, weatherY)
+end
+
+#all(isnan.(Array(data_weatherX)))
+
+begin
+    mRFR = machine(RandomForestRegressor(n_trees = 500), weatherX, weatherY);
+    evaluate!(mRFR, measure = rmse)
+    mRFR.report
+end
+
+begin
+    pred_RFR= enemy_of_out_of_bounds(predict(mRFR, weatherX))
+    rmse_RFR=rmse(pred_RFR, weatherY)
+end
+
+begin
+    mNNR= machine(NeuralNetworkRegressor(builder = MLJFlux.Short(n_hidden = 69,
+                                                                 dropout = 0.5,
+                                                                 σ = tanh),
+                                         Loss=  ,
+                                         optimiser=ADAMW(),
+                                         epochs = 100, 
+                                         batch_size = 20),
+                                         data_weatherX, data_weatherY);
+    evaluate!(mNNR, measure = rmse)
+end
+
+begin
+    pred_NNR= enemy_of_out_of_bounds(predict(mNNR, weatherX))
+    rmse_NNR=rmse(pred_NNR, weatherY)
+end
+
+
+
+begin
+    mNNR_std= machine( NeuralNetworkRegressor(builder = MLJFlux.Linear(σ=tanh),
+                                         optimiser=ADAMW(),
+                                         epochs = 100, 
+                                         batch_size = 20),
+                                         data_weatherX, data_weatherY);
+    evaluate!(mNNR_std, measure = rmse)
+end
+
+begin
+    pred_NNR_std= enemy_of_out_of_bounds(predict(mNNR_std, weatherX))
+    rmse_NNR_std=rmse(pred_NNR_std, weatherY)
+end
+
+begin
+    mNNC= NeuralNetworkClassifier(builder = MLJFlux.Short(n_hidden = 100,
+                                                                 dropout = 0.1,
+                                                                 σ = MLJFlux.Flux.relu),
+                                         epochs = 100, 
+                                         batch_size = 20);
+
+    mach=machine(mNNC,weatherX,weatherY)
+
+end
+
+begin
+    pred_NNC= enemy_of_out_of_bounds(predict(mNNC, weatherX))
+    rmse_NNC=rmse(pred_NNC, weatherY)
+end
+
+NeuralNetworkClassifier(builder,finaliser,optimiser,loss,epochs,batch_size,lambda,alpha,rng,optimiser_changes_trigger_retraining,acceleration)
+
+
+begin
+    nn = Chain(Dense(1, 50, tanh), Dense(50, 2))
+    function loss(x, y)
+        output = nn(x)
+        m = output[1, :]
+        s = softplus.(output[2, :])
+        mean((m .- y) .^ 2 ./ (2 * s) .+ log.(s))
+    end
+    opt = ADAMW()
+    p = Flux.params(nn) # these are the parameters to be adapted.
+    data = Flux.DataLoader((data_weatherX, data_weatherY), batchsize = 32)
+    for _ in 1:50
+        Flux.Optimise.train!(loss, p, data, opt)
+    end
+end
+
